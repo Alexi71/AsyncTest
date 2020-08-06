03845a96db1b3a0ef84094c8cd5b62c0c659960e
Selina Kuhnen <selina.kuhnen@noris-group.com>
2020-08-06 15:14:06 +0200

    aufgeräumt

--- AsyncSample/Form1.cs
+++ AsyncSample/Form1.cs
@@ -21,7 +21,6 @@ namespace AsyncSample
 
             _thread = new Thread(DoMe);
             _worker = new BackgroundWorker();
-            _worker.DoWork += _worker_DoWork;
             _worker.WorkerReportsProgress = true;
             _worker.ProgressChanged += (sender, args) =>
             {
@@ -29,26 +28,12 @@ namespace AsyncSample
                 label1.Text = progress.ToString();
             };
 
-            _worker.RunWorkerCompleted += (sender, args) =>
-            {
-                MessageBox.Show("Habe Fertig");
-            };
-
             //_thread.Start();
-        }
-
-        private void _worker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            for (var i = 0; i < 1000; i++)
-            {
-                _worker.ReportProgress(i);
-                Thread.Sleep(100);
-            }
         }
 
         private void DoMe()
         {
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i <= 10; i++)
             {
                 lock (this)
                 {
@@ -69,32 +54,9 @@ namespace AsyncSample
             label1.Text = i.ToString();
         }
 
-        private async void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "Started...";
-            //await GetLoop();
-            //label1.Text = "Completed...";
-
-            //_worker.RunWorkerAsync();
             _thread.Start();
-        }
-
-        static void DoWork(int i)
-        {
-            Console.WriteLine(i + " ");
-        }
-
-        private async Task GetLoop()
-        {
-            //await Task.Run(() =>
-            //{
-            //    for (var i = 0; i < 1000000; i++)
-            //    {
-            //        Console.WriteLine(i);
-            //    }
-            //});
-            await Task.Delay(10000);
-
         }
     }
 }
